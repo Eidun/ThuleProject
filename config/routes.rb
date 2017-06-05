797e2ea5e5dd8d6c10fb860569968efc6344160b
Miguel <mff1993@hotmail.com>
2017-06-05 20:53:39 +0200

    Fix the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'example#index'
   get 'example' => 'example#index'
   get 'example/new' => 'example#new'
   post 'example/create' => 'example#create'
